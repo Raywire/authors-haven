7dd51bc48e0001c54d35bb421a4bc0de3298ca6f
Ryan Wire Simiyu <raywire@Ryans-MacBook-Pro.local>
2019-05-06 00:59:49 +0300

    Signs out a user

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { authSignOut } from '../../helpers/authSignOut';
 
 const SignedInLinks = () => {
   return (
     <div>
       <ul className="right">
         <li><NavLink to="/create">New Article</NavLink></li>
-        <li><NavLink to="/">Log Out</NavLink></li>
+        <li><a href="/signin" onClick={authSignOut}>Log Out</a></li>
         <li><NavLink to="/" className="btn btn-floating pink lighten-1">RW</NavLink></li>
       </ul>
     </div>
---
+++ src/helpers/authSignOut.js
@@ -0,0 +1,3 @@
+export const authSignOut = () => {
+    localStorage.removeItem('user');
+};
